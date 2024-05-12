ad7e8c3ab767c203895f7e7a70d9c1af1de01f35
Nghia <tnghia130217@gmail.com>
2024-05-13 02:55:08 +0700

    move addCart functon into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,21 @@
 export const cart = [];
+
+
+
+export function addToCart(productId){
+	let matchinIitem;
+
+	cart.forEach((cartItem)=>{
+		if(productId=== cartItem.productId){
+			matchinIitem =cartItem
+		}
+	})
+	if(matchinIitem){
+		matchinIitem.quantity+=1;
+	}else{
+		cart.push({
+			productId,
+			quantity:1,
+		})
+	}
+}
