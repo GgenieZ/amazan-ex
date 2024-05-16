f68173d32808185b2de53b09f15e5ae04512063d
Nghia <tnghia130217@gmail.com>
2024-05-17 03:10:07 +0700

    create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,8 @@
 import {renderOrderSummary} from "./check out/orderSumary.js";
 import {renderPaymentSummary} from "./check out/paymentSumary.js"
 import {updateCheckOut} from "../data/cart.js";
+import '../data/cart-oop.js'
 
 updateCheckOut();
-
-
 renderOrderSummary();
 renderPaymentSummary();
